c1a34042b8f5d4e179e21feeda506f472a995238
Anderson Andrade <139899277+AndersonCCandrade@users.noreply.github.com>
2024-06-05 14:47:55 -0300

    Tarefa 2 - Criar a classe ItemDePedido

---
+++ Modelos/Produtos/ItemDePedido.cs
@@ -0,0 +1,17 @@
+﻿namespace Csharp_Comex.Modelos.Produtos;
+
+internal class ItemDePedido
+{
+    public Produto Produto { get; }
+    public int Quantidade { get; }
+    public double PrecoUnitario { get; }
+    public double SubTotal { get; }
+
+    public ItemDePedido(Produto produto, int quantidade)
+    {
+        Produto = produto;
+        Quantidade = quantidade;
+        PrecoUnitario = produto.Preco;
+        SubTotal= quantidade * PrecoUnitario;
+    }
+}
